31f2b8aa8b6f5be7a1cae1d9c66021fd7effa50d
edouard3443454 <93147216+edouard3443454@users.noreply.github.com>
2021-12-03 13:05:04 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 let piéton = 0
 function jaune () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P2, 0)
     pins.digitalWritePin(DigitalPin.P1, 1)
 }
 function pétion () {
@@ -12,42 +14,41 @@ function pétion () {
         orange2()
     }
 }
+input.onButtonPressed(Button.A, function () {
+	
+})
 function blanche2 () {
     pins.digitalWritePin(DigitalPin.P16, 0)
 }
 function orange2 () {
-    pins.digitalWritePin(DigitalPin.P8, 1)
-}
-function jaune2 () {
-    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P8, 0)
 }
 function feux_de_circulation () {
-    rouge2()
+    orange()
     vert()
     basic.pause(5000)
-    vert2()
     jaune()
     basic.pause(2000)
-    jaune2()
     rouge()
     basic.pause(2000)
 }
-function rouge2 () {
-    pins.digitalWritePin(DigitalPin.P2, 0)
-}
 function orange () {
-    pins.digitalWritePin(DigitalPin.P8, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+    pins.digitalWritePin(DigitalPin.P8, 1)
 }
 function blanche () {
+    pins.digitalWritePin(DigitalPin.P2, 1)
     pins.digitalWritePin(DigitalPin.P16, 1)
-}
-function vert2 () {
-    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P8, 0)
 }
 function rouge () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P0, 0)
     pins.digitalWritePin(DigitalPin.P2, 1)
 }
 function vert () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 0)
     pins.digitalWritePin(DigitalPin.P0, 1)
 }
 basic.forever(function () {
